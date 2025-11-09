cff1f33607d7d5667317bc02afd715e6637d6683
ArisSgouros <arissgouros@gmail.com>
2025-11-09 21:31:05 +0200

    example(wf/cij): update

--- examples/run_cij_lammps/run_cij.py
+++ examples/run_cij_lammps/run_cij.py
@@ -1,7 +1,9 @@
-from simuglue.workflow.cij.run import run_cij
+from simuglue.workflow.cij.run import init_cij, run_cij, parse_cij
 from simuglue.workflow.cij.post import post_cij
 cases = ["cij_2d.yaml", "cij_3d.yaml"]
 for case_ in cases:
     print(f"case: {case_}")
+    init_cij(config_path=case_)
     run_cij(config_path=case_)
+    parse_cij(config_path=case_)
     post_cij(config_path=case_)
