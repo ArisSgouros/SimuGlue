dc7599ca0afccec2aec48cb59d6e05691c25f442
ArisSgouros <arissgouros@gmail.com>
2025-11-11 14:30:28 +0200

    feat: rm slg-hello/template

--- src/simuglue/template/__init__.py
+++
@@ -1,3 +0,0 @@
-# src/simuglue/template/__init__.py
-from .template import template_function
-__all__ = ["template_function"]
--- src/simuglue/template/template.py
+++
@@ -1,4 +0,0 @@
-from __future__ import annotations
-
-def template_function(template_string: str) -> str:
-    return template_string
