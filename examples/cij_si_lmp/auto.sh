48b6ddcfce6b59fbfa83bf81e235f5713afc4b60
ArisSgouros <arissgouros@gmail.com>
2026-01-13 15:50:40 +0200

    example(cij_si_lmp): upd

--- examples/cij_si_lmp/auto.sh
+++ examples/cij_si_lmp/auto.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sgl wf cij run --config cij_2d.yaml
-sgl wf cij post --config cij_2d.yaml
-sgl wf cij run --config cij_3d.yaml
-sgl wf cij post --config cij_3d.yaml
+sgl wf cij all --config cij_2d.yaml
+sgl wf cij all --config cij_3d.yaml
