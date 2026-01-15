3afdecfb122a8fcad3fd24991253f05b1dc5a672
ArisSgouros <arissgouros@gmail.com>
2026-01-15 11:28:23 +0200

    build: rm workflow/cij_01

--- src/simuglue/workflow/cij_01/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/simuglue/workflows/cij/__init__.py
-from .runner import run_cij  # public API
-
-# auto-register workflow-local backends
-from .backends import lammps
-#from .backends import qe
