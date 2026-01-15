5ba80545b6daefbee819531064d13a91ee99c3fe
ArisSgouros <arissgouros@gmail.com>
2026-01-15 11:30:37 +0200

    build: rm test manual_build_crystalbuilder (deprec)

--- tests/manual_build_crystalbuilder/nacl/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CMD="sgl build crystalbuilder"
-
-$CMD \
-  in.basis_nacl.dat "5,5,5" --file_pos="o.nacl.dat" --file_dump="o.nacl.lammpstrj" --file_xyz="o.nacl.xyz" --rc="2.8201" --bond=1 > o.log
--- tests/manual_build_crystalbuilder/ni/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CMD="sgl build crystalbuilder"
-
-$CMD \
-  in.basis_ni.dat "4,4,4" --file_pos="o.ni.dat" --file_dump="o.ni.lammpstrj" --file_xyz="o.ni.xyz" --rc="2.49" --bond=1 > o.log
